fece590aedca1cec53e11f27d9f23465ce844831
Gustavo Silva <gustavooficial2511@gmail.com>
2026-01-11 18:32:20 -0300

    feat: add requirements

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL)
 local_session = sessionmaker(autoflush=False, bind=engine)
 
     
